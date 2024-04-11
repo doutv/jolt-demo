b8f48b9621c6055f895125214ce70853d3e595da
Jason.Huang <jason.huang@okg.com>
2024-04-11 18:35:40 +0800

    RSA not work, possible reason: https://github.com/RustCrypto/RSA/issues/390

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -17,28 +17,16 @@ fn fib(n: u32) -> u128 {
     b
 }
 
-/// Verify Schnorr Signature
-/// Trace length: 169422912
+/// Verify RSA Signature
+/// Trace length:
 #[jolt::provable(stack_size = 100000, memory_size = 10000000)]
-fn schnorr(message: &[u8], verifying_key_bytes: &[u8], signature_bytes: &[u8]) {
-    use k256::schnorr::{Signature, signature::Verifier, VerifyingKey};
-    let verifying_key = VerifyingKey::from_bytes(&verifying_key_bytes).unwrap();
+fn rsa(message: &[u8], verifying_key_bytes: &[u8], signature_bytes: &[u8]) {
+    use rsa::pkcs1v15::VerifyingKey;
+    use rsa::signature::Verifier;
+    use rsa::{pkcs1::DecodeRsaPublicKey, pkcs1v15::Signature, sha2::Sha256};
+    let verifying_key: VerifyingKey<Sha256> = VerifyingKey::from_pkcs1_der(&verifying_key_bytes).unwrap();
     let signature = Signature::try_from(signature_bytes).unwrap();
     verifying_key
         .verify(message, &signature)
         .unwrap();
 }
-
-// /// Verify RSA Signature
-// /// Trace length:
-// #[jolt::provable(stack_size = 100000, memory_size = 10000000)]
-// fn rsa(message: &[u8], verifying_key_bytes: &[u8], signature_bytes: &[u8]) {
-//     use rsa::pkcs1v15::VerifyingKey;
-//     use rsa::signature::Verifier;
-//     use rsa::{pkcs1::DecodeRsaPublicKey, pkcs1v15::Signature, sha2::Sha256};
-//     let verifying_key: VerifyingKey<Sha256> = VerifyingKey::from_pkcs1_der(&verifying_key_bytes).unwrap();
-//     let signature = Signature::try_from(signature_bytes).unwrap();
-//     verifying_key
-//         .verify(message, &signature)
-//         .unwrap();
-// }
--- src/main.rs
+++ src/main.rs
@@ -15,54 +15,22 @@ pub fn main() {
     });
 
     {
-        // Schnorr Signature
-        use k256::schnorr::{signature::Signer, SigningKey};
-        use rand_core::OsRng; // requires 'getrandom' feature
-
-        let message = b"Schnorr signatures prove knowledge of a secret in the random oracle model";
-        // Signing
-        let signing_key = SigningKey::random(&mut OsRng); // serialize with `.to_bytes()`
-        let verifying_key_bytes = signing_key.verifying_key().to_bytes(); // 32-bytes
-        let signature = signing_key.sign(message); // returns `k256::schnorr::Signature`
-        let signature_bytes = signature.to_bytes(); // 64-bytes
-        print_time("native Schnorr verify", || {
-            guest::execute_schnorr(
-                message,
-                verifying_key_bytes.as_slice(),
-                signature_bytes.as_slice(),
-            );
-        });
-        let (prove_schnorr, _verify_schnorr) = guest::build_schnorr();
-        print_time("zkVM Schnorr verify", || {
-            let (_output, _proof) = prove_schnorr(
-                message,
-                verifying_key_bytes.as_slice(),
-                signature_bytes.as_slice(),
-            );
-        });
-        // let is_valid = verify_schnorr(proof);
-        // println!("output: {}", output);
-        // println!("valid: {}", is_valid);
-    }
-    /*
-    {
         // RSA Signature
         use rsa::pkcs1v15::{SigningKey, VerifyingKey};
         use rsa::pkcs1::EncodeRsaPublicKey;
         use rsa::sha2::{Digest, Sha256};
         use rsa::signature::{Keypair, RandomizedSigner, SignatureEncoding, Verifier};
         use rsa::RsaPrivateKey;
-
-        let mut rng = rand::thread_rng();
+        use rand_core::OsRng;
 
         let bits = 2048;
-        let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+        let private_key = RsaPrivateKey::new(&mut OsRng, bits).expect("failed to generate a key");
         let signing_key = SigningKey::<Sha256>::new(private_key);
         let verifying_key = signing_key.verifying_key();
 
         // Sign
         let data = b"hello world";
-        let signature = signing_key.sign_with_rng(&mut rng, data);
+        let signature = signing_key.sign_with_rng(&mut OsRng, data);
         assert_ne!(signature.to_bytes().as_ref(), data.as_slice());
         let signature_bytes = signature.to_bytes();
         let verifying_key_bytes = verifying_key.to_pkcs1_der().unwrap();
@@ -83,5 +51,4 @@ pub fn main() {
             );
         });
     }
-    */
 }
