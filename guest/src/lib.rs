16941e380e2f0747da34682d6939ab6bf8014b65
Dumitrel Loghin <dumitrel.loghin@okg.com>
2024-04-11 18:09:09 +0800

    finish schnorr

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,8 +1,8 @@
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
 
-use k256::schnorr::Signature;
-
+/// Fibonacci(50)
+/// Trace length: 1301
 #[jolt::provable]
 fn fib(n: u32) -> u128 {
     let mut a: u128 = 0;
@@ -17,16 +17,28 @@ fn fib(n: u32) -> u128 {
     b
 }
 
-use k256::schnorr::{signature::Verifier, VerifyingKey};
-
-#[jolt::provable]
+/// Verify Schnorr Signature
+/// Trace length: 169422912
+#[jolt::provable(stack_size = 100000, memory_size = 10000000)]
 fn schnorr(message: &[u8], verifying_key_bytes: &[u8], signature_bytes: &[u8]) {
-    //
-    // Verification
-    //
+    use k256::schnorr::{Signature, signature::Verifier, VerifyingKey};
     let verifying_key = VerifyingKey::from_bytes(&verifying_key_bytes).unwrap();
     let signature = Signature::try_from(signature_bytes).unwrap();
     verifying_key
         .verify(message, &signature)
-        .expect("invalid signature");
+        .unwrap();
 }
+
+// /// Verify RSA Signature
+// /// Trace length:
+// #[jolt::provable(stack_size = 100000, memory_size = 10000000)]
+// fn rsa(message: &[u8], verifying_key_bytes: &[u8], signature_bytes: &[u8]) {
+//     use rsa::pkcs1v15::VerifyingKey;
+//     use rsa::signature::Verifier;
+//     use rsa::{pkcs1::DecodeRsaPublicKey, pkcs1v15::Signature, sha2::Sha256};
+//     let verifying_key: VerifyingKey<Sha256> = VerifyingKey::from_pkcs1_der(&verifying_key_bytes).unwrap();
+//     let signature = Signature::try_from(signature_bytes).unwrap();
+//     verifying_key
+//         .verify(message, &signature)
+//         .unwrap();
+// }
